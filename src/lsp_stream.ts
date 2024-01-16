208665fdfedab7d1be5bec01141b368c6a445a14
kamecha <masakikameyama81@gmail.com>
2024-01-16 23:43:54 +0900

    refactor: read LSP Header

--- src/lsp_stream.ts
+++ src/lsp_stream.ts
@@ -10,10 +10,7 @@ export class LspDecoderStream extends TransformStream<
   LSP.RequestMessage
 > {
   #buffer = new Uint8Array();
-  #header: LSPHeader = {
-    length: 0,
-  };
-  #headerFindFlag = false;
+  #header?: LSPHeader
   // https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#headerPart
   #headerDecoder = new TextDecoder("ascii");
   #contentDecoder = new TextDecoder("utf-8");
@@ -21,7 +18,7 @@ export class LspDecoderStream extends TransformStream<
     super({
       transform: (chunk, controller) => {
         this.#buffer = new Uint8Array([...this.#buffer, ...chunk]);
-        if (!this.#headerFindFlag) {
+        if (this.#header === undefined) {
           // Content-Length:20\r\n
           // Content-Type:application/vscode-jsonrpc; charset=utf-8\r\n ← これは任意
           // \r\n
@@ -34,12 +31,14 @@ export class LspDecoderStream extends TransformStream<
             // ここでエラー投げるのもあり
             return;
           }
-          this.#header.length = Number(headerMatch[1]);
-          this.#headerFindFlag = true;
+          this.#header = {
+            length: Number(headerMatch[1]),
+            type: headerMatch[2],
+          }
           // #bufferをheaderの分だけ削除
           this.#buffer = this.#buffer.slice(headerMatch[0].length);
         }
-        if (this.#headerFindFlag) {
+        if (this.#header !== undefined) {
           if (this.#buffer.length >= this.#header.length) {
             const content = this.#contentDecoder.decode(
               this.#buffer.slice(0, this.#header.length),
@@ -50,7 +49,6 @@ export class LspDecoderStream extends TransformStream<
             // controller.enqueue(content);
             // #bufferをcontentの分だけ削除
             this.#buffer = this.#buffer.slice(this.#header.length);
-            this.#headerFindFlag = false;
           }
         }
       },
