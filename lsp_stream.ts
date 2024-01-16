022d840bfa089ed202aedeb9aab168c05942c521
kamecha <masakikameyama81@gmail.com>
2024-01-16 17:35:15 +0900

    一旦ログ削除

--- lsp_stream.ts
+++ lsp_stream.ts
@@ -4,7 +4,6 @@ export class LspDecoderStream extends TransformStream<Uint8Array, string> {
   constructor() {
     super({
       transform: (chunk, controller) => {
-        logger().info(new TextDecoder("utf-8").decode(chunk));
         controller.enqueue(new TextDecoder("utf-8").decode(chunk));
       },
     });
@@ -15,7 +14,6 @@ export class LspEncoderStream extends TransformStream<string, Uint8Array> {
   constructor() {
     super({
       transform: (chunk, controller) => {
-        logger().info(chunk);
         controller.enqueue(new TextEncoder().encode(chunk));
       },
     });
