e67f03cdff877046df4767ab868e85a384aa84ca
kamecha <masakikameyama81@gmail.com>
2024-01-16 23:46:55 +0900

    fix: fix LSP module version

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
 export * as log from "https://deno.land/std@0.209.0/log/mod.ts";
-export * as LSP from "npm:vscode-languageserver-protocol";
+export * as LSP from "npm:vscode-languageserver-protocol@3.17.5";
 export { assertEquals } from "https://deno.land/std@0.212.0/assert/assert_equals.ts";
